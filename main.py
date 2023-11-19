58bc64ec4020ea56f744d9d74d84affa671e3a97
Sylvan Franklin <sylvanfranklin@icloud.com>
2023-11-19 17:57:07 -0500

    types update

--- main.py
+++ main.py
@@ -5,10 +5,36 @@ which may not render on some systems, especially Windows, but are worth a shot
 """
 # I imported this in order to make it more clear that the board is an array of arrays
 from typing import List
+from enum import Enum
+
+
+# I used this enum to define the different states a space could be
+class Player(Enum):
+    X = "X"
+    O = "O"
+    EMPTY = " "
+
+
+# I made this to represent the different ways that you can win
+WIN_PATTERNS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+
+# pass this function a board, and it will tell you if X, O, or neither wins
+def check_win(board: List[Player]) -> Player:
+    for pattern in WIN_PATTERNS:
+        # the pattern object is an array indicies, checking that all indicies are the same will tell us if someone has three in a row
+        if all([board[i] == Player.X for i in pattern]):
+            return Player.X
+        elif all([board[i] == Player.O for i in pattern]):
+            return Player.O
+
+    return Player.EMPTY
 
 
 # prints that board, given a 2d array (an array of arrays), the outer array represents the big board, and the innner the smaller boards
-def print_board(board: List[List[str]]):
+
+
+def print_board(board: List[List[Player]]):
     # This print statement adds some blank space before the board
     print("\n" * 2)
 
@@ -18,7 +44,7 @@ def print_board(board: List[List[str]]):
         for x in range(3):
             for k in range(3):
                 for j in range(3):
-                    print(f" {board[k+(y*3)][j+(x*3)]} ", end="")
+                    print(f" {board[k + (y * 3)][j + (x * 3)].value} ", end="")
 
                 if k != 2:
                     print("│", end="")
@@ -33,23 +59,16 @@ def print_board(board: List[List[str]]):
 
 
 if __name__ == "__main__":
-    # This is the board, which I made with a list comprehension (just a faster way to write:
-    # board = [] 
-    # for _ in range(9):
-    #     sub_board = []
-    #     for _ in range(9):
-    #         sub_board.append("x")
-
-    #     board.append(sub_board)
-
-    
-    board = [["x" for _ in range(9)] for _ in range(9)]
- 
-    # Here I'm just calling the function, in the future we will do it in a loop with input validation, and update the board state with each move. 
-    # For example if we wanted to place an "o", in board 5, space 6 you could do board[5][6] = "o"
+    # here is what making a big board and printing it looks like
+    board = [[Player.X for _ in range(9)] for _ in range(9)]
     print_board(board)
 
-
-
+    # this is just so that I could check the check win function. You can mess with what is x / o and what is empty to get different results
+    lil_board = [
+        Player.X, Player.EMPTY, Player.EMPTY,
+        Player.X, Player.EMPTY, Player.EMPTY,
+        Player.X, Player.EMPTY, Player.EMPTY,
+    ]
+    print(f"{check_win(lil_board).value} wins")
 
 
