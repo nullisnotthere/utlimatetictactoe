eb14b8cac4c15c002dfe18eb1cbccf1ad7512da7
dpizura <150277531+dpizura@users.noreply.github.com>
2023-12-06 16:58:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ which may not render on some systems, especially Windows, but are worth a shot
 # I imported this in order to make it more clear that the board is an array of arrays
 from typing import List
 from enum import Enum
+import random
 
 
 # I used this enum to define the different states a space could be
@@ -16,16 +17,7 @@ class Player(Enum):
 
 
 # I made this to represent the different ways that you can win
-WIN_PATTERNS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-]
+WIN_PATTERNS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 
 # pass this function a board, and it will tell you if X, O, or neither wins
@@ -67,18 +59,123 @@ def print_board(board: List[List[Player]]):
     print("\n" * 2)
 
 
-if __name__ == "__main__":
-    board = [[Player.X for _ in range(9)] for _ in range(9)]
+def player_turn(x, y):
+    #print mark
+ '''   
+123
+456
+789
 
-    while True:
-        player_status = input(
-            "Will you be playing with a second player or against the computer?"
-        )
-        if player_status.lower() == "second player":
-            first_move_grid = 10
-            while first_move_grid not in range(1, 10):
-                first_move = input("Pick a grid for your first move (1-9): ")
-        elif player_status.lower() == "computer":
-            pass
-        else:
-            print("enter a valid choice, player or computer")
+147
+258
+369
+
+159
+357
+'''  
+def win(d):
+    for v in d.values():
+        if 1 in lst:
+            if 2 in lst:
+                if 3 in lst:
+                    return win = True
+            if 4 in lst:
+                if 7 in lst:
+                    return win = True
+            if 5 in lst:
+                if 9 in lst:
+                    return win = True
+        if 2 in lst:
+            if 5 in lst:
+                if 8 in lst:
+                    return win = True
+        if 3 in lst:
+            if 5 in lst:
+                if 7 in lst:
+                    return win = True
+            if 6 in lst:
+                if 9 in lst:
+                    return win = True
+        if 4 in lst:
+            if 5 in lst:
+                if 6 in lst:
+                    return win = True
+        if 7 in lst:
+            if 8 in lst:
+                if 9 in lst:
+                    return win = True
+        return False
+    
+    
+
+
+if __name__ == "__main__":
+    # here is what making a big board and printing it looks like
+    board = [[Player.X for _ in range(9)] for _ in range(9)]
+    print_board(board)
+
+    # this is just so that I could check the check win function. You can mess with what is x / o and what is empty to get different results
+    lil_board = [
+        Player.X, Player.EMPTY, Player.EMPTY,
+        Player.X, Player.EMPTY, Player.EMPTY,
+        Player.X, Player.EMPTY, Player.EMPTY,
+    ]
+    print(f"{check_win(lil_board).value} wins")
+
+    p1_MOVES_DCT = {'1':[], '2':[], '3':[], '4':[], '5':[], '6':[], '7':[], '8':[], '9':[]}
+    p2_MOVES_DCT = {'1':[], '2':[], '3':[], '4':[], '5':[], '6':[], '7':[], '8':[], '9':[]}
+    
+    while False:
+        PLAYER_STATUS = input("Will you be playing with a "second player" or against the "computer"?")
+        FIRST_MOVE_GRID = 10
+        while FIRST_MOVE_GRID not in range(1, 10):
+            FIRST_MOVE_GRID = int(input("Pick a grid for your first move (1-9)."))
+        P1_FIRST_MOVE = 10
+        while P1_FIRST_MOVE not in range(1, 10):
+            P1_FIRST_MOVE = int(input("Pick a spot in grid {FIRST_MOVE_GRID} for your first move (1-9)."))
+        player_turn(FIRST_MOVE_GRID, P1_FIRST_MOVE) #Makes Move
+        p1_MOVES_DCT[f'{FIRST_MOVE_GRID}'].append(P1_FIRST_MOVE) #Records Play
+        if PLAYER_STATUS.lower() == "second player": 
+            P2_FIRST_MOVE = 10
+            while P2_FIRST_MOVE not in range(1, 10):
+                P2_FIRST_MOVE = int(input("Player 2, pick a spot in grid {P1_FIRST_MOVE} for your first move (1-9)."))
+            player_turn(P1_FIRST_MOVE, P2_FIRST_MOVE)
+            p2_MOVES_DCT[f'{P1_FIRST_MOVE}'].append(P2_FIRST_MOVE)
+            P2_FIRST_MOVE = PREVIOUS_MOVE
+            while Win = False:
+                P1_MOVE = 10
+                while P1_MOVE not in range(1, 10):
+                    P1_MOVE = int(input("Player 1, pick a spot in grid {PREVIOUS_MOVE} for your next move (1-9)."))
+                player_turn(PREVIOUS_MOVE, P1_MOVE)
+                p1_MOVES_DCT[f'PREVIOUS_MOVE'].append(P1_MOVE)
+                P1_MOVE = PREVIOUS_MOVE
+                P2_MOVE = 10
+                while P2_MOVE not in range(1, 10):
+                    P2_MOVE = int(input("Player 2, pick a spot in grid {PREVIOUS_MOVE} for your next move (1-9)."))
+                player_turn(PREVIOUS_MOVE, P2_MOVE)
+                p2_MOVES_DCT[f'PREVIOUS_MOVE'].append(P2_MOVE)
+                P2_MOVE = PREVIOUS_MOVE
+        elif PLAYER_STATUS.lower() == "computer":
+            True
+    
+    while PLAYER_STATUS.lower() == "computer":
+        P2_FIRST_MOVE = random.randint(range(1, 10))
+        player_turn(P1_FIRST_MOVE, P2_FIRST_MOVE)
+        P2_FIRST_MOVE = PREVIOUS_MOVE
+        while Win = False:
+            P1_MOVE = 10
+            while P1_MOVE not in range(1, 10):
+                P1_MOVE = int(input("Player 1, pick a spot in grid {PREVIOUS_MOVE} for your next move (1-9)."))
+            player_turn(PREVIOUS_MOVE, P1_MOVE)
+            p1_MOVES_DCT[f'PREVIOUS_MOVE'].append(P1_MOVE)
+            P1_MOVE = PREVIOUS_MOVE
+            P2_MOVE = random.randint(range(1, 10))
+            player_turn(PREVIOUS_MOVE, P2_MOVE)
+            p2_MOVES_DCT[f'PREVIOUS_MOVE'].append(P2_MOVE)
+            P2_MOVE = PREVIOUS_MOVE
+        
+        
+        
+
+    
+    
