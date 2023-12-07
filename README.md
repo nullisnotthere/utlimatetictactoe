5f74ed95c1a151236d334b72cc5e1705caab30b6
Sylvan Franklin <sylvanfranklin@icloud.com>
2023-12-07 14:33:49 -0500

    final changes

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-Dylan Pizura and Sylvan Franklin final project for CS1210. 
+# UVM CS1210 Final Project 
+`Dylan Pizura and Sylvan Franklin`
+
+#### Dependencies
+- python standard library `random`, `typing`, `enum`
+- unix terminal emulator with ANSI rendering for color and clear codes
+
+#### Run instructions
+The game will prompt you on whether you want to play against another player or play against a random AI. It will then ask you to make a move on a grid. If you are unfamiliar with the rules of ultimate tic tac toe I recommend watching a short youtube video [here](https://www.youtube.com/shorts/_Na3a1ZrX7c). But the basic rules are as follows:
+
+1. player one makes a move on one of nine smaller boards
+2. player two makes a move on the corresponding big board
+3. repeat steps **1** and **2** until a board is won
+4. when three boards in a row are won, the game is over. 
+
+In the event that a board is full you may choose to move anywhere. 
+
 
 
      
--- main.py
+++ main.py
@@ -24,7 +24,9 @@ class fg:
     white = "\u001b[37m"
     reset = "\u001b[0m"
 
+
 CLEAR = "\033[2J"
+
 
 # I used this enum to define the different states a space could be
 class Player(Enum):
@@ -34,12 +36,12 @@ class Player(Enum):
 
 
 class Board:
-    def __init__(self, board):
+    def __init__(self, board, win_status):
         self.board = board
-        self.WIN_STATUS = Player.EMPTY
+        self.WIN_STATUS = win_status
 
 
-# I made this to represent the different ways that you can win
+# different coordinates for a win
 WIN_PATTERNS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -52,15 +54,26 @@ WIN_PATTERNS = [
 ]
 
 
+def win(active):
+    print_board(board)
+    print(
+        f"{fg.blue}{active.value}{fg.reset} Wins! {fg.green}Thank you for Playing{fg.reset}",
+    )
+    sys.exit()
+
+
 # pass this function a board, and it will tell you if X, O, or neither wins
 
 
-def check_win(board: List[Player]) -> Player:
+def check_win(b: Board) -> Player:
+    if b.WIN_STATUS != Player.EMPTY:
+        return Player.EMPTY
+
     for pattern in WIN_PATTERNS:
         # the pattern object is an array indicies, checking that all indicies are the same will tell us if someone has three in a row
-        if all([board[i] == Player.X for i in pattern]):
+        if all([b.board[i] == Player.X for i in pattern]):
             return Player.X
-        elif all([board[i] == Player.O for i in pattern]):
+        elif all([b.board[i] == Player.O for i in pattern]):
             return Player.O
 
     return Player.EMPTY
@@ -85,7 +98,21 @@ def print_board(board):
         for x in range(3):
             for k in range(3):
                 for j in range(3):
-                    print(f" {board[k + (y * 3)].board[j + (x * 3)].value} ", end="")
+                    status = board[k + (y * 3)].WIN_STATUS
+                    cell = board[k + (y * 3)].board[j + (x * 3)]
+
+                    if status == Player.X:
+                        if cell == Player.X:
+                            print(f" {fg.green}{cell.value}{fg.reset} ", end="")
+                        else:
+                            print(f" {fg.red}{cell.value}{fg.reset} ", end="")
+                    elif status == Player.O:
+                        if cell == Player.O:
+                            print(f" {fg.green}{cell.value}{fg.reset} ", end="")
+                        else:
+                            print(f" {fg.red}{cell.value}{fg.reset} ", end="")
+                    else:
+                        print(f" {cell.value} ", end="")
 
                 if k != 2:
                     print("│", end="")
@@ -98,44 +125,71 @@ def print_board(board):
     print("\n" * 2)
 
 
-def make_move(active: Player, PREVIOUS_MOVE: int):
-    while True:
-        cell = 10
-        try:
-            while cell not in range(1, 10):
-                cell = int(
-                    input(
-                        f"{fg.green}[{active.value} to play]{fg.reset} pick a spot in grid {fg.blue}{PREVIOUS_MOVE}{fg.reset} for your move [1-9]: "
-                    )
-                )
+# get a cell value, that is numeric, in the correct range, and not already taken
 
-            break
+
+def choose_new_board():
+    while True:
+        try:
+            grid = int(input(f"pick a grid for your move [1-9]: "))
+            if grid in range(1, 10):
+                target = board[grid - 1].board
+                if all([c != Player.EMPTY for c in target]):
+                    return grid
+                else:
+                    print("That grid is not empty")
+            else:
+                print(f"{grid} is not in range [1-9]")
+
         except ValueError:
             print(f"{fg.red}Invalid move entered!{fg.reset}")
 
 
-        target = board[PREVIOUS_MOVE - 1].board[cell - 1]
-        if target == Player.EMPTY:
-            board[PREVIOUS_MOVE - 1].board[cell - 1] = active
+def make_move(active: Player, PREVIOUS_MOVE: int):
+    # special case where board the you are trying to move in is full
+    if all([c != Player.EMPTY for c in board[PREVIOUS_MOVE - 1].board]):
+        print(f"This board is full, {active.value} pick a new one")
+        PREVIOUS_MOVE = choose_new_board()
 
-            if check_win(board[PREVIOUS_MOVE - 1].board):
-                board[PREVIOUS_MOVE-1].WIN_STATUS = active.value
-            return cell
+    while True:
+        try:
+            cell = int(
+                input(
+                    f"{fg.green}[{active.value} to play]{fg.reset} pick a spot in grid {fg.blue}{PREVIOUS_MOVE}{fg.reset} for your move [1-9]: "
+                )
+            )
 
-        else:
-            print(f"Please enter a {fg.red} NON-EMPTY {fg.reset} cell.")
+            if cell in range(1, 10):
+                target = board[PREVIOUS_MOVE - 1].board[cell - 1]
+                if target == Player.EMPTY:
+                    board[PREVIOUS_MOVE - 1].board[cell - 1] = active
+                    if check_win(board[PREVIOUS_MOVE - 1]) != Player.EMPTY:
+                        board[PREVIOUS_MOVE - 1].WIN_STATUS = active
+                    return cell
+                else:
+                    print(f"{fg.yellow }That cell is not empty {fg.reset}")
+            else:
+                print(f"{fg.red}{cell} is not in range [1-9]{fg.reset}")
 
-
+        except ValueError:
+            print(f"{fg.red}Invalid move entered!{fg.reset}")
 
 
 def make_move_computer(active: Player, PREVIOUS_MOVE: int):
+
+
     while True:
+        # case where the board is full
+        while all(i != Player.EMPTY for i in board[PREVIOUS_MOVE - 1].board):    
+            PREVIOUS_MOVE = random.randint(1, 9)
+
         cell = random.randint(1, 9)
         target = board[PREVIOUS_MOVE - 1].board[cell - 1]
+
         if target == Player.EMPTY:
             board[PREVIOUS_MOVE - 1].board[cell - 1] = active
-            if check_win(board[PREVIOUS_MOVE - 1].board):
-                board[PREVIOUS_MOVE - 1].WIN_STATUS = active.value
+            if check_win(board[PREVIOUS_MOVE - 1]) != Player.EMPTY:
+                board[PREVIOUS_MOVE - 1].WIN_STATUS = active
             break
 
     return cell
@@ -148,70 +202,54 @@ def flop_player(player: Player):
 
 
 if __name__ == "__main__":
-    board: List[Board] = [Board([Player.EMPTY for _ in range(9)]) for _ in range(9)]
+    print(CLEAR)
+    board: List[Board] = [
+        Board([Player.EMPTY for _ in range(9)], Player.EMPTY) for _ in range(9)
+    ]
 
     while True:
         player_status = input(
             "Will you be playing with a second player locally (L) or against the computer (C)? "
         ).upper()
 
-
-        if player_status.lower() == "c":
-
+        if player_status.lower() == "c" or player_status.lower == "computer":
             print_board(board)
             active_player = random.choice((Player.X, Player.O))
-            first_move_grid = 10
-            while first_move_grid not in range(1, 10):
-                first_move_grid = int(input("Pick a grid for your first move [1-9]: "))
+            PREVIOUS_MOVE = 10
 
-            PREVIOUS_MOVE = make_move(active_player, first_move_grid)
-            print_board(board)
-            active_player = flop_player(active_player)
-            Win = False
-            while Win == False:
+            while PREVIOUS_MOVE not in range(1, 10):
+                PREVIOUS_MOVE = int(input("Pick a grid for your first move [1-9]: "))
+
+            while True:
                 print_board(board)
                 PREVIOUS_MOVE = make_move(active_player, PREVIOUS_MOVE)
-                active_player = flop_player(active_player)
-                print(check_win_board(board))
                 if check_win_board(board) != Player.EMPTY:
-                    print(f"{active_player} wins!")
-                    Win = True
+                    win(active_player)
 
-                print_board(board)
+                active_player = flop_player(active_player)
+                # active_player = flop_player(active_player)
                 PREVIOUS_MOVE = make_move_computer(active_player, PREVIOUS_MOVE)
-                active_player = flop_player(active_player)
-                print(check_win_board(board))
                 if check_win_board(board) != Player.EMPTY:
-                    print(f"{active_player} wins!")
-                    Win = True
+                    win(active_player)
 
-            print("Thank you for playing")
-            sys.exit()
+                active_player = flop_player(active_player)
 
         elif player_status.lower() == "l":
-
             print_board(board)
             active_player = random.choice((Player.X, Player.O))
             first_move_grid = 10
             while first_move_grid not in range(1, 10):
                 first_move_grid = int(input("Pick a grid for your first move [1-9]: "))
-            
+
             PREVIOUS_MOVE = make_move(active_player, first_move_grid)
-
-
-            print_board(board)
             active_player = flop_player(active_player)
-            PREVIOUS_MOVE = make_move(active_player, PREVIOUS_MOVE)
 
             Win = False
             while Win == False:
-
                 print_board(board)
                 PREVIOUS_MOVE = make_move(active_player, PREVIOUS_MOVE)
-                active_player = flop_player(active_player)
-                if check_win_board(board) != Player.EMPTY:
-                    print(f"{active_player} wins!")
-                    Win = True
 
-            print("Thank you for playing")
-            sys.exit()
+                if check_win_board(board) != Player.EMPTY:
+                    win(active_player)
+
+                active_player = flop_player(active_player)
